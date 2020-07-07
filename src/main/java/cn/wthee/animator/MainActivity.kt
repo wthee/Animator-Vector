28fa74296a266d66f44308b2a1678104b368a899
wthee <3116728602@qq.com>
2020-07-07 17:17:55 +0800

    final

--- src/main/java/cn/wthee/animator/MainActivity.kt
+++ src/main/java/cn/wthee/animator/MainActivity.kt
@@ -1,15 +1,19 @@
 package cn.wthee.animator
 
 import android.graphics.drawable.AnimatedVectorDrawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var behavior: BottomSheetBehavior<View>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +22,11 @@ class MainActivity : AppCompatActivity() {
         var isBack = true
         val btn = findViewById<ImageButton>(R.id.btn)
         btn.setOnClickListener {
+            if(isBack){
+                hideBottomSheet()
+            }else{
+                showBottomSheet()
+            }
             btn.isClickable = false
             val drawable = btn.drawable
             if (drawable is AnimatedVectorDrawable){
@@ -35,5 +44,27 @@ class MainActivity : AppCompatActivity() {
                 btn.isClickable = true
             }
         }
+
+        // The View with the BottomSheetBehavior
+        val coordinatorLayout = findViewById<View>(R.id.cl)
+        val bottomSheet: View = coordinatorLayout.findViewById(R.id.bottom_sheet)
+        behavior = BottomSheetBehavior.from(bottomSheet)
+        behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+//                TODO("Not yet implemented")
+            }
+        })
+    }
+
+    fun showBottomSheet() {
+        behavior.state = BottomSheetBehavior.STATE_EXPANDED
+    }
+
+    fun hideBottomSheet() {
+        behavior.state = BottomSheetBehavior.STATE_COLLAPSED
     }
 }
